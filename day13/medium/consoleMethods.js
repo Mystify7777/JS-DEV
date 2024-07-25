f1a75cc59d13b1a37cdfc3fbbfc2c30d930d90d5
Aryan Kumar <aryankumar.55566@gmail.com>
2024-07-25 15:06:22 +0530

    Day 13 assignment Level 2 Commpleted

---
+++ day13/medium/consoleMethods.js
@@ -0,0 +1,9 @@
+/*
+1. 10 > 2 * 10 use console.assert()
+2. Write a warning message using console.warn()
+3. Write an error message using console.error() 
+*/
+console.assert(10 > 2 * 10);
+console.assert(10 * 2 > 10);
+console.warn("This is a warning message");
+console.error("This is an error message");
